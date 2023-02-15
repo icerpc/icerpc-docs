69a89dd3b0f7314d64c8bc57a781920281b7e55d
Reece Humphreys <reecewh@icloud.com>
2023-02-15 14:57:04 -0500

    Fix page history to correctly update

--- components/Shell/PageHistory.tsx
+++ components/Shell/PageHistory.tsx
@@ -8,10 +8,14 @@ import { sideBarData, baseUrls } from 'data/side-bar-data';
 import { SideBarLink, isCategory } from 'types';
 import { useVersionContext } from 'context/state';
 
+const stripTrailingSlash = (str: string) => {
+  return str.endsWith('/') ? str.slice(0, -1) : str;
+};
+
 export function PageHistory() {
   // Get the data for the next and previous links
   const { version } = useVersionContext();
-  const path = useRouter().asPath;
+  const path = stripTrailingSlash(useRouter().asPath);
   const baseUrl = baseUrls.find((item) => path.startsWith(item))!;
   const data: SideBarLink[] = sideBarData(baseUrl, version).flatMap((item) => {
     if (isCategory(item)) {
@@ -22,32 +26,30 @@ export function PageHistory() {
   });
 
   // Find the next and previous pages in the sidebar if they exist
-  const index = data.map((item) => item.path).indexOf(path);
-  const previous: SideBarLink | null = data[index - 1];
-  const next: SideBarLink | null = data[index + 1];
+  const index = data.map((item) => stripTrailingSlash(item.path)).indexOf(path);
+  const previous = index > 0 ? data[index - 1] : undefined;
+  const next = index < data.length - 1 ? data[index + 1] : undefined;
 
   return (
-    <>
-      <div className="mx-[-1rem] mt-12 mb-0 flex flex-row justify-between p-0">
-        {previous ? (
-          <Link href={previous.path}>
-            <div className="flex h-10 flex-row items-center gap-2 rounded p-4 text-center text-primary hover:bg-[#E9F1FE]">
-              <FaChevronLeft size={12} />
-              {previous.title}
-            </div>
-          </Link>
-        ) : (
-          <div></div>
-        )}
-        {next && (
-          <Link href={next.path}>
-            <div className="flex h-10 flex-row items-center gap-2 rounded p-4 text-center text-primary hover:bg-[#E9F1FE]">
-              {next.title}
-              <FaChevronRight size={12} />
-            </div>
-          </Link>
-        )}
-      </div>
-    </>
+    <div className="mx-[-1rem] mt-12 mb-0 flex flex-row justify-between p-0">
+      {previous ? (
+        <Link href={previous.path}>
+          <div className="flex h-10 flex-row items-center gap-2 rounded p-4 text-center text-primary hover:bg-[#E9F1FE]">
+            <FaChevronLeft size={12} />
+            {previous.title}
+          </div>
+        </Link>
+      ) : (
+        <div></div>
+      )}
+      {next && (
+        <Link href={next.path}>
+          <div className="flex h-10 flex-row items-center gap-2 rounded p-4 text-center text-primary hover:bg-[#E9F1FE]">
+            {next.title}
+            <FaChevronRight size={12} />
+          </div>
+        </Link>
+      )}
+    </div>
   );
 }
