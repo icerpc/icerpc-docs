3cb988719b2e009c99689a540438421a198ea607
Reece Humphreys <reecewh@icloud.com>
2023-03-02 16:43:37 -0500

    Fix frontmatter parsing bug

--- markdoc/nodes/document.markdoc.ts
+++ markdoc/nodes/document.markdoc.ts
@@ -6,22 +6,5 @@ import { Document } from 'components/Nodes/Document';
 export default {
   ...nodes.document,
   render: Document,
-  attributes: nodes.document.attributes,
-  transform(node: Node, config: Config) {
-    const frontmatter = node.attributes.frontmatter
-      .split('\n')
-      .reduce(
-        (
-          acc: { [x: string]: any },
-          line: { split: (arg0: string) => [any, any] }
-        ) => {
-          const [key, value] = line.split(':');
-          acc[key] = value.trim();
-          return acc;
-        },
-        {}
-      );
-    const children = node.transformChildren(config) ?? [];
-    return new Tag(`${this.render}`, { frontmatter: frontmatter }, children);
-  }
+  attributes: nodes.document.attributes
 };
