0e5a960f11006c661e7d566b5984cf39d507bc30
Reece Humphreys <reece@zeroc.com>
2023-04-17 11:13:54 -0400

    Make main title smaller for mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@
 export const Hero = () => {
   return (
     <div className="flex flex-col text-center">
-      <h1 className="mb-2	 text-8xl font-extrabold leading-tight tracking-tighter dark:text-white">
+      <h1 className="mb-2	text-6xl font-extrabold leading-tight tracking-tighter dark:text-white md:text-8xl">
         <span className="bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">
           Network
         </span>{' '}
