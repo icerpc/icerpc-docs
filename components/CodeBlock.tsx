052e30dce1f84448934294896f30b168e02a2fda
Reece Humphreys <reece@zeroc.com>
2023-05-03 13:48:47 -0400

    Improved codeblock

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,18 +1,15 @@
 // Copyright (c) ZeroC, Inc.
 
-import { ReactNode, useState } from 'react';
-import { BiCopy } from 'react-icons/bi';
+import { ReactNode } from 'react';
 import { FaFile } from 'react-icons/fa';
 import { BsTerminalFill } from 'react-icons/bs';
 import { clsx } from 'clsx';
-import copy from 'copy-to-clipboard';
-import Highlight, { Language, defaultProps } from 'prism-react-renderer';
-// @ts-ignore
-import Prism from 'prism-react-renderer/prism';
+import { Highlight, themes, Prism } from 'prism-react-renderer';
 import dynamic from 'next/dynamic';
-import { IconContext } from 'react-icons';
 import { useEncoding } from 'context/state';
 import { Encoding } from 'types';
+import { CopyButton } from './CopyButton';
+
 const MermaidDiagram = dynamic(() => import('components/Mermaid'), {
   ssr: false
 });
@@ -71,7 +68,6 @@ export const CodeBlock = ({
   title,
   addEncoding
 }: Props) => {
-  const [copied, setCopied] = useState(false);
   const { encoding } = useEncoding();
 
   // If the code is a slice file, add the encoding to the first line if the current
@@ -84,22 +80,6 @@ export const CodeBlock = ({
     children = encodingLines.join('\n').concat(children);
   }
 
-  // Split the code into lines
-  const lines =
-    typeof children === 'string' ? children.split('\n').filter(Boolean) : [];
-
-  // If the code is a command line, add a prompt to the first line
-  // If language is undefined or if it is not included in commandLineLanguages, render a file icon
-  // Otherwise, render a terminal icon
-  const languageIcon =
-    language === undefined ? (
-      <FaFile />
-    ) : commandLineLanguages.includes(language) ? (
-      <BsTerminalFill />
-    ) : (
-      <FaFile />
-    );
-
   // If the language is mermaid, render the mermaid diagram
   if (language?.toLowerCase() === 'mermaid') {
     return (
@@ -111,113 +91,76 @@ export const CodeBlock = ({
 
   return (
     // Container for the code block
-    <div className="group relative my-6 items-center">
-      <div className="w-full rounded-lg bg-[#17232d]">
-        {language != undefined && (
-          <TopBar
-            languageIcon={languageIcon}
-            language={language}
-            lines={lines}
-            setCopied={setCopied}
-            copied={copied}
-            title={title}
-          >
-            {children}
-          </TopBar>
+    <div className="group relative my-4 w-full items-center text-clip rounded-lg border border-[rgb(46,46,46)] bg-[rgb(6,22,38)]">
+      <TopBar language={language} code={children} title={title}>
+        {children}
+      </TopBar>
+      <Highlight
+        language={language ?? ''}
+        code={children?.trim()}
+        theme={themes.jettwaveDark}
+      >
+        {({ className, tokens, getLineProps, getTokenProps, style }) => (
+          <pre className={clsx(className, 'my-3')} style={style}>
+            {tokens.map((line, i) => (
+              <div
+                key={i}
+                {...getLineProps({ line })}
+                className="max-w-0 px-5 py-[3px] text-xs"
+              >
+                {line.map((token, key) => (
+                  <span
+                    key={key}
+                    {...getTokenProps({ token, key })}
+                    className="table-cell"
+                  />
+                ))}
+              </div>
+            ))}
+          </pre>
         )}
-        {/* PrismJS styled code block*/}
-        <Highlight
-          {...defaultProps}
-          code={children?.trim()}
-          language={language as Language}
-          theme={undefined}
-        >
-          {({ className, tokens, getLineProps, getTokenProps, style }) => (
-            <pre
-              className={clsx(
-                className,
-                'm-0 my-1 overflow-scroll rounded-lg px-4 py-3 text-left'
-              )}
-              style={style}
-            >
-              {tokens.map((line, i) => (
-                <div
-                  key={i}
-                  {...getLineProps({ line, key: i })}
-                  className={clsx(className)}
-                >
-                  <LineContent>
-                    {line.map((token, key) => (
-                      <span key={key} {...getTokenProps({ token, key })} />
-                    ))}
-                  </LineContent>
-                </div>
-              ))}
-            </pre>
+      </Highlight>
+      {title == undefined && (
+        <div
+          className={clsx(
+            'absolute right-0 top-2 mr-4 rounded border border-[rgb(46,46,46)] bg-[rgb(6,22,38)] opacity-0',
+            'transition-opacity duration-500 group-hover:opacity-100'
           )}
-        </Highlight>
-      </div>
+        >
+          <CopyButton text={children} />
+        </div>
+      )}
     </div>
   );
-};
-
-type LineContentProps = {
-  children: ReactNode;
-};
-
-const LineContent = ({ children }: LineContentProps) => {
-  return <div className="table-cell max-w-0 py-[3px] text-xs">{children}</div>;
 };
 
 type TopBarProps = {
-  languageIcon: ReactNode;
   language?: string;
   title?: string;
-  lines: string[];
-  setCopied: (copied: boolean) => void;
-  copied: boolean;
+  code: string;
   children: ReactNode;
 };
 
-const TopBar = ({
-  languageIcon,
-  language,
-  lines,
-  title,
-  setCopied,
-  copied
-}: TopBarProps) => {
-  // If the language is csharp, change it to C#
-  // TODO: Add more languages
-  if (language?.toLowerCase() === 'csharp') {
-    language = 'C#';
-  }
-
-  return (
-    <div className="flex h-8 flex-row justify-between text-white">
-      <div className="m-0 ml-4 flex flex-row items-center gap-2 p-0 text-xs">
-        {languageIcon}
-        {title ?? language}
+const TopBar = ({ language, code, title }: TopBarProps) =>
+  language ? (
+    <div className="flex h-12 flex-row items-center justify-between border-b border-b-[hsl(0,0%,18%)] text-white">
+      <div className="m-0 ml-4 flex flex-row items-center gap-3 p-0 text-sm">
+        {LanguageIcon(language ?? '')}
+        {title ?? language ?? ''}
       </div>
-      <button
-        aria-label="Copy to clipboard"
-        className={`mr-4 ${
-          lines.length === 1 ? 'top-[2px]' : 'top-[0.1rem]'
-        } hover:text-gray-400 `}
-        onClick={() => {
-          copy(lines.join('\n'));
-          setCopied(true);
-          setTimeout(() => setCopied(false), 3000);
-        }}
-      >
-        {copied ? (
-          '🎉'
-        ) : (
-          <IconContext.Provider value={{ size: '1em' }}>
-            <BiCopy />
-          </IconContext.Provider>
-        )}
-      </button>
+      <div className="mr-4 flex flex-row items-center gap-4">
+        <CopyButton text={code} />
+      </div>
     </div>
+  ) : null;
+
+// Provides an icon given the language of the code block
+function LanguageIcon(language: string) {
+  return language === undefined ? (
+    <FaFile />
+  ) : commandLineLanguages.includes(language) ? (
+    <BsTerminalFill />
+  ) : (
+    <FaFile />
   );
-};
+}
