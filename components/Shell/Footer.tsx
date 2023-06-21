56281080c8c4d45b1410cb6b0b840ff31449065a
Reece Humphreys <reece@zeroc.com>
2023-06-21 16:36:59 -0400

    Change order in copyright notice

--- components/Shell/Footer.tsx
+++ components/Shell/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
     <footer className="-mb-2 mt-0 border-t border-lightBorder py-6 dark:border-darkBorder dark:bg-black">
       <div className="mx-auto w-full max-w-[100rem] px-4 md:flex md:items-center md:justify-between md:px-10">
         <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
-          © ZeroC {new Date().getFullYear()}
+          © {new Date().getFullYear()} ZeroC
         </span>
         <ul className="mr-10 mt-3 flex flex-wrap items-center text-sm font-medium sm:mt-0">
           <li>
