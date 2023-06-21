b32bfb681355160fb2778e295536db20f1f98913
Reece Humphreys <reece@zeroc.com>
2023-06-21 15:11:46 -0400

    Update SliceSelector to use tooltips (#79)

--- components/SliceSelector.tsx
+++ components/SliceSelector.tsx
@@ -4,9 +4,10 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { Tab } from '@headlessui/react';
 import clsx from 'clsx';
-
+import { Tooltip } from 'flowbite-react';
 import { useEncoding } from 'context/state';
 import { Encoding, encodings } from 'types';
+import { AppLink } from './Nodes/AppLink';
 
 export const SliceSelector = () => {
   const { encoding: currentEncoding, setEncoding } = useEncoding();
@@ -31,12 +32,9 @@ export const SliceSelector = () => {
 
   return (
     <>
-      <div className="mb-6 w-full pl-1 pr-6">
-        <h2 className="py-2 text-xs font-semibold uppercase text-slate-800 underline decoration-lightBorder underline-offset-[10px] dark:text-white dark:decoration-darkBorder">
-          Slice encoding:
-        </h2>
+      <div className="mb-6 w-full pr-6">
         <Tab.Group selectedIndex={selectedIndex} onChange={onChange}>
-          <Tab.List className="my-4 flex gap-0 space-x-1 rounded-2xl bg-transparent">
+          <Tab.List className="my-4 flex rounded-xl bg-transparent">
             {encodings.map((encoding, index) => (
               <EncodingTab
                 key={encoding}
@@ -59,7 +57,7 @@ type EncodingTabProps = {
 
 const EncodingTab = ({ encoding, selected }: EncodingTabProps) => {
   const className = clsx(
-    'w-full rounded-xl border-[1.5px] bg-white p-2 text-xs font-medium uppercase leading-tight',
+    'mx-1 w-[108px] rounded-lg border-[1.5px] bg-white p-2 text-center text-xs font-medium uppercase leading-tight',
     'focus:outline-none focus:ring-0',
     'transition-shadow duration-300 ease-in-out hover:scale-[1.01] hover:shadow-lg',
     'dark:bg-[#32363c] dark:text-white',
@@ -67,6 +65,28 @@ const EncodingTab = ({ encoding, selected }: EncodingTabProps) => {
       ? 'border border-primary bg-white text-primary dark:border-white dark:text-primary'
       : 'bg-slate-50 text-slate-500 hover:bg-white/80 hover:text-primary dark:border-darkBorder dark:text-white/40'
   );
+  const tooltipContent =
+    encoding === Encoding.Slice1 ? (
+      <p>
+        Slice1 should only be used when doing interop with ZeroC Ice, to read
+        learn more visit{' '}
+        <AppLink href="/docs/icerpc-for-ice-users/slice/one-syntax-two-encodings">
+          here
+        </AppLink>
+        .
+      </p>
+    ) : (
+      <p>
+        Slice2 is the default encoding for IceRPC and recommended for new
+        projects.
+      </p>
+    );
 
-  return <Tab className={className}>{encoding}</Tab>;
+  return (
+    <Tooltip content={tooltipContent} placement="bottom" className="w-56">
+      <Tab as="div" className={className}>
+        {encoding}
+      </Tab>
+    </Tooltip>
+  );
 };
