67b7184379fce66e8bb91038dfadc2e11867062a
Reece Humphreys <reece@zeroc.com>
2023-07-19 10:40:36 -0400

    Disable link prefetching for API links and refactor AppLink (#140)

--- components/Nodes/AppLink.tsx
+++ components/Nodes/AppLink.tsx
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router';
 import { Mode } from 'types';
 import { useMode } from 'context/state';
 
-type Props = {
+type AppLinkProps = {
   href: string;
   target?: string;
   className?: string;
@@ -15,102 +15,126 @@ type Props = {
   children: ReactNode;
 };
 
-export const AppLink = (props: Props) => {
+// Set of classes to apply when the link is an API link.
+const apiClasses = [
+  'after:ml-[3px] after:mr-[1px] after:rounded-sm after:bg-primary after:px-[2px] after:py-[1px]',
+  "after:align-middle after:text-[8px] after:font-semibold after:text-white after:content-['API']",
+  'dark:after:bg-primary/80'
+];
+
+// Default styles for the link.
+const defaultStyle: CSSProperties = { textUnderlineOffset: '5px' };
+
+export const AppLink = ({
+  href: originalHref,
+  target: originalTarget,
+  className = 'font-medium text-primary hover:text-[rgb(64,131,193)]',
+  style: originalStyle,
+  children
+}: AppLinkProps) => {
   const router = useRouter();
   const { mode, setMode } = useMode();
-  const currentPagePath = router.asPath; // Use router.asPath as currentPagePath if not provided
 
-  const resolveRelativeLink = (href: string) => {
-    if (
-      href.startsWith('http://') ||
-      href.startsWith('https://') ||
-      href.startsWith('/')
-    ) {
-      return href; // Absolute link, return as is
-    }
+  const href = isApiLink(originalHref)
+    ? resolveApiLink(originalHref)
+    : resolveRelativeLink(originalHref, router.asPath);
 
-    if (href.startsWith('#')) {
-      const currentPath = currentPagePath.split('#')[0];
-      return `${currentPath}${href}`;
-    }
+  const style = { ...defaultStyle, ...originalStyle };
 
-    const currentDir = currentPagePath.replace(/\/[^/]+$/, '');
-    const resolvedPath = `${currentDir}/${href}`;
-    return resolvedPath;
-  };
-
-  const href = isApiLink(props.href)
-    ? resolveApiLink(props.href)
-    : resolveRelativeLink(props.href); // Use resolveRelativeLink for href
-
-  // Rest of the code remains the same
+  // Determine the target for the link, e.g., "_blank" for external links.
   const target =
-    props.target ||
-    (props.href.startsWith('http') || isApiLink(props.href)
+    originalTarget ||
+    (isExternalLink(originalHref) || isApiLink(originalHref)
       ? '_blank'
       : undefined);
-  const style = props.style || {
-    textUnderlineOffset: '5px'
+
+  const shouldPrefetch = !isApiLink(originalHref);
+
+  /**
+   * Handles the click on the link, specifically for slice links.
+   */
+  const handleLinkClick = () => {
+    if (isSliceLink(href)) {
+      const hrefMode = href.includes('slice1') ? Mode.Slice1 : Mode.Slice2;
+      if (hrefMode !== mode) {
+        setMode(hrefMode);
+      }
+    }
   };
 
   return (
     <Link
-      {...props}
       href={href}
       target={target}
       rel={target === '_blank' ? 'noreferrer' : undefined}
-      onClick={() => {
-        // If the user navigates to a link with a different mode, update the mode
-        if (isSliceLink(href)) {
-          // Get the segments of the href path
-          const pathSegments = href.split('/');
-          const hrefMode =
-            pathSegments.find(
-              (segment) => segment === 'slice1' || segment === 'slice2'
-            ) === 'slice1'
-              ? Mode.Slice1
-              : Mode.Slice2;
-
-          // If the href mode is different from the current mode, update the mode
-          if (hrefMode !== mode) {
-            setMode(hrefMode);
-          }
-        }
-      }}
+      prefetch={shouldPrefetch}
+      onClick={handleLinkClick}
       className={clsx(
-        props.className ??
-          'font-medium text-primary hover:text-[rgb(64,131,193)]',
-        href.includes('docs.testing.zeroc.com/api') && [
-          'after:ml-[3px] after:mr-[1px] after:rounded-sm after:bg-primary after:px-[2px] after:py-[1px]',
-          "after:align-middle after:text-[8px] after:font-semibold after:text-white after:content-['API']",
-          'dark:after:bg-primary/80'
-        ]
+        className,
+        href.includes('docs.testing.zeroc.com/api') && apiClasses
       )}
       style={style}
     >
-      {props.children}
+      {children}
     </Link>
   );
 };
 
-const isSliceLink = (href: string) => {
-  // Check if the link contains .../slice1/... or .../slice2/... or slice1/... or slice2/...
-  return href.match(/(^|\/)slice[1-2]\//);
-};
+// Utility Functions
 
+/**
+ * Check if a link is external.
+ * @param {string} href - The link to check.
+ * @returns {boolean} - True if it's an external link, false otherwise.
+ */
+const isExternalLink = (href: string) =>
+  href.startsWith('http://') || href.startsWith('https://');
+
+/**
+ * Determines if a link is a slice link.
+ * @param {string} href - The link to check.
+ * @returns {boolean} - True if it's a slice link, false otherwise.
+ */
+const isSliceLink = (href: string) => /(^|\/)slice[1-2]\//.test(href);
+
+/**
+ * Determines if a link is an API link.
+ * @param {string} href - The link to check.
+ * @returns {boolean} - True if it's an API link, false otherwise.
+ */
 const isApiLink = (href: string) => {
-  const languages = ['csharp', 'rust'];
+  const supportedLanguages = ['csharp', 'rust'];
   return (
-    languages.some((lang) => href.startsWith(`${lang}:`)) &&
+    supportedLanguages.some((lang) => href.startsWith(`${lang}:`)) &&
     href.split(':').length > 1
   );
 };
 
+/**
+ * Resolves an API link to its full URL.
+ * @param {string} href - The original API link.
+ * @returns {string} - The resolved API link.
+ */
 const resolveApiLink = (href: string) => {
-  const [lang, ...rest] = href.split(':');
-  const [module, method] = rest.join('.').split('#'); // split module and method names
-  const apiHref = `https://docs.testing.zeroc.com/api/${lang}/api/${module}.html${
+  const [language, ...rest] = href.split(':');
+  const [module, method] = rest.join('.').split('#');
+  return `https://docs.testing.zeroc.com/api/${language}/api/${module}.html${
     method ? `#${method}` : ''
-  }`; // include "#" and method name if it exists
-  return apiHref;
+  }`;
+};
+
+/**
+ * Resolve relative links to their full path.
+ * @param {string} href - The original link.
+ * @param {string} routerPath - The current route path.
+ * @returns {string} - The resolved link.
+ */
+const resolveRelativeLink = (href: string, routerPath: string) => {
+  if (isExternalLink(href) || href.startsWith('/')) {
+    return href;
+  }
+  if (href.startsWith('#')) {
+    return `${routerPath.split('#')[0]}${href}`;
+  }
+  return `${routerPath.replace(/\/[^/]+$/, '')}/${href}`;
 };
