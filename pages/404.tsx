b5befa2a2ebd522de40d5dff43913f68c4ceecf0
Reece Humphreys <reece@zeroc.com>
2023-07-19 15:55:34 -0400

    Explicit theme check in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,10 @@
 // Copyright (c) ZeroC, Inc.
 
+import { useTheme } from 'next-themes';
 import Error from 'next/error';
+import { Theme } from 'types';
 
 export default function NotFound() {
-  return <Error statusCode={404} />;
+  const { resolvedTheme } = useTheme();
+  return <Error statusCode={404} withDarkMode={resolvedTheme === Theme.Dark} />;
 }
