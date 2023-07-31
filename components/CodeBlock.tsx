01c906f0a78fc17039d30ae78b76c5f13a0899f7
Joe George <joe@zeroc.com>
2023-07-31 14:56:54 -0400

    Move Slice syntax highlighter to its own file

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -29,6 +29,7 @@ require('prismjs/components/prism-bash');
 require('prismjs/components/prism-ebnf');
 require('prismjs/components/prism-protobuf');
 require('utils/prism-ice');
+require('utils/prism-slice');
 
 const commandLineLanguages = [
   'bash',
@@ -40,31 +41,6 @@ const commandLineLanguages = [
   'dos',
   'shell'
 ];
-
-Prism.languages.slice = {
-  keyword: /\b(interface|module|struct|class|exception|enum|throws|compact)\b/,
-  comment: [
-    {
-      pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
-      lookbehind: true,
-      greedy: true
-    },
-    {
-      pattern: /(^|[^\\:])\/\/.*/,
-      lookbehind: true,
-      greedy: true
-    }
-  ],
-  string: {
-    pattern: /(["'])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
-    greedy: true
-  },
-  number: /\b0x[\da-f]+\b|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?/i,
-  operator: /[<>]=?|[!=]=?=?|--?|\+\+?|&&?|\|\|?|[?*/~^%]/,
-  punctuation: /[{}[\];(),.:]/,
-  builtin:
-    /\b(?:bool|int8|uint8|int16|uint16|int32|uint32|varint32|varuint32|int64|uint64|varint62|varuint62|float32|float64|string|Slice1|Slice2)\b/
-};
 
 type Props = {
   children: string;
---
+++ utils/prism-slice.js
@@ -0,0 +1,26 @@
+(function (Prism) {
+    Prism.languages.slice = {
+        keyword: /\b(interface|module|struct|class|exception|enum|throws|compact)\b/,
+        comment: [
+            {
+                pattern: /(^|[^\\])\/\*[\s\S]*?(?:\*\/|$)/,
+                lookbehind: true,
+                greedy: true
+            },
+            {
+                pattern: /(^|[^\\:])\/\/.*/,
+                lookbehind: true,
+                greedy: true
+            }
+        ],
+        string: {
+            pattern: /(["'])(?:\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
+            greedy: true
+        },
+        number: /\b0x[\da-f]+\b|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:e[+-]?\d+)?/i,
+        operator: /[<>]=?|[!=]=?=?|--?|\+\+?|&&?|\|\|?|[?*/~^%]/,
+        punctuation: /[{}[\];(),.:]/,
+        builtin:
+            /\b(?:bool|int8|uint8|int16|uint16|int32|uint32|varint32|varuint32|int64|uint64|varint62|varuint62|float32|float64|string|Slice1|Slice2)\b/
+    };
+}(Prism));
