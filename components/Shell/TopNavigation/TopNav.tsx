650ee0ff1973c22db7c6988922d29b976f4effe4
Reece Humphreys <reece@zeroc.com>
2023-08-17 11:36:22 -0400

    Fix top navigation item hover

--- components/Shell/TopNavigation/TopNav.tsx
+++ components/Shell/TopNavigation/TopNav.tsx
@@ -136,9 +136,9 @@ const TopNavigationItem = ({ name, href }: TopNavigationItemProps) => {
   const prefetch = href.startsWith('http') ? false : undefined;
 
   const baseClassName =
-    'mx-3 overflow-hidden whitespace-nowrap dark:text-[rgba(255,255,255,0.6)]';
+    'mx-3 overflow-hidden whitespace-nowrap dark:text-[rgba(255,255,255,0.6)] hover:text-zinc-900 dark:hover:text-white';
   const activeClassName =
-    'mx-3 text-primary font-semibold no-underline decoration-2 underline-offset-[1.5rem] opacity-100 dark:text-white';
+    'mx-3 text-primary font-semibold no-underline decoration-2 underline-offset-[1.5rem] opacity-100 dark:text-white hover:!text-primary dark:hover:!text-white';
 
   const [linkClassName, setLinkClassName] = useState(baseClassName);
 
