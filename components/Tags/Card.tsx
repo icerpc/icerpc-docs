045e83cc2b9f66aec0aa39bc2a31835d82b189cb
Reece Humphreys <reece@zeroc.com>
2023-08-24 10:47:49 -0400

    Remove dead code

--- components/Tags/Card.tsx
+++ components/Tags/Card.tsx
@@ -23,29 +23,3 @@ export const Card = ({ title, description, href }: CardProps) => {
     </AppLink>
   );
 };
-
-// type MiniLinkProps = {
-//   title: string;
-//   link: string;
-// };
-
-// export const MiniLink = ({ title, link }: MiniLinkProps) => {
-//   return (
-//     <a
-//       href={link ? link : '#'}
-//       key={title}
-//       className="flex h-[100px] flex-col justify-center gap-2 rounded-md border-[1px] bg-white p-4 transition-shadow duration-300  ease-in-out hover:scale-[1.01] hover:shadow-lg dark:border-darkBorder dark:bg-black"
-//       target="_blank"
-//       rel="noreferrer"
-//     >
-//       <h4 className="m-0 pt-[0.5rem] font-semibold dark:text-white">{title}</h4>
-//       <IconContext.Provider value={{ size: '1em' }}>
-//         <div className="flex flex-row items-center gap-2">
-//           <BsGithub className="dark:text-white/80" />
-//           <span style={{ fontSize: '25px' }}>&middot;</span>
-//           <SiDotnet className="dark:text-white/80" size={30} />
-//         </div>
-//       </IconContext.Provider>
-//     </a>
-//   );
-// };
