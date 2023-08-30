3743bae4ab9bb5250c40e29e70bbbda1ca93e180
Reece Humphreys <reece@zeroc.com>
2023-08-30 15:02:25 -0400

    Fix build

--- markdoc/tags/index.ts
+++ markdoc/tags/index.ts
@@ -12,7 +12,6 @@ import sideBySide from './side-by-side.markdoc';
 export { default as homeTitle } from './home-title.markdoc';
 export { default as callout } from './callout.markdoc';
 export { default as divider } from './divider.markdoc';
-export { default as modes } from './modes.markdoc';
 export { default as grid } from './grid.markdoc';
 export { default as title } from './title.markdoc';
 export { slice1, slice2 } from './mode-section.markdoc';
