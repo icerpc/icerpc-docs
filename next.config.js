e33495d491fb8e26282e1db31dab90396481e256
Reece Humphreys <reece@zeroc.com>
2023-09-18 12:02:57 -0400

    Fix build and cleanup

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
-const rulesToProcess = [/\.m?js/, /\.(js|cjs|mjs)$/].map(String);
-
 module.exports = {
   pageExtensions: ['tsx'],
   output: 'standalone',
   webpack: (config, {}) => {
     // Ignore the tools directory
-    config.module.rules = config.module.rules.map((rule) => {
-      if (rule !== '...' && rulesToProcess.indexOf(String(rule.test)) > -1) {
-        rule.exclude = ['/tools/'];
-      }
-      return rule;
+    config.module.rules.push({
+      test: /tools/,
+      loader: 'ignore-loader'
     });
     return config;
   }
