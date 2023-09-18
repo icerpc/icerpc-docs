0acac7795e51e9984263e5c5280d1d7a1fd6f31e
Reece Humphreys <reece@zeroc.com>
2023-09-18 11:47:53 -0400

    Ignore tools when doing build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+const rulesToProcess = [/\.m?js/, /\.(js|cjs|mjs)$/].map(String);
+
 module.exports = {
   pageExtensions: ['tsx'],
-  output: 'standalone'
+  output: 'standalone',
+  webpack: (config, {}) => {
+    // Ignore the tools directory
+    config.module.rules = config.module.rules.map((rule) => {
+      if (rule !== '...' && rulesToProcess.indexOf(String(rule.test)) > -1) {
+        rule.exclude = ['/tools/'];
+      }
+      return rule;
+    });
+    return config;
+  }
 };
