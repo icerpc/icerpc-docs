a29620d86dbdafcea6ae8157118d5d9ff11f4391
Reece Humphreys <reece@zeroc.com>
2023-09-26 09:57:43 -0400

    Add console.log back

--- tools/pdf/generatePdf.ts
+++ tools/pdf/generatePdf.ts
@@ -224,7 +224,7 @@ async function mergePdfs(
   pdfDoc.setCreationDate(new Date());
 
   // Serialize and save
-  ('Saving merged PDF...');
+  console.log('Saving merged PDF...');
   const mergedPdfBytes = await pdfDoc.save();
   fs.writeFileSync(`${outputDir}/merged_document.pdf`, mergedPdfBytes);
 
