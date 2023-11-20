5d967c4b87cc0598c4a3ccbd41ba3d76131c10bc
Reece Humphreys <reece@zeroc.com>
2023-11-20 10:49:29 -0500

    Import ImageResponse from next/og

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,6 +1,7 @@
 // Copyright (c) ZeroC, Inc.
 
-import { NextRequest, NextResponse, ImageResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { ImageResponse } from 'next/og';
 import { getBreadcrumbs } from 'lib/breadcrumbs';
 
 export async function GET(request: NextRequest) {
