c376fd47fe810cc1cfe9ba0badfcb26d622d52e6
Reece Humphreys <reece@zeroc.com>
2023-11-21 15:37:29 -0500

    package-lock fixes

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,8 +1,7 @@
 // Copyright (c) ZeroC, Inc.
 
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse, ImageResponse } from 'next/server';
 import { getBreadcrumbs } from 'lib/breadcrumbs';
-import { ImageResponse } from 'next/og';
 
 export async function GET(request: NextRequest) {
   try {
