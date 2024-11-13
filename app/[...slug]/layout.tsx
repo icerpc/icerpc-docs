9c1088eb714ff1b4abe3e6b29ca9d84312b24394
Joe George <joe@zeroc.com>
2024-11-13 15:52:23 -0500

    Fix build

--- app/[...slug]/layout.tsx
+++ app/[...slug]/layout.tsx
@@ -3,6 +3,6 @@
 import { PathProvider } from 'context/state';
 
 export default function DocsLayout(props: any) {
-  const path = '/' + props.params.slug?.join('/') ?? '';
+  const path = '/' + (props.params.slug?.join('/') ?? '');
   return <PathProvider path={path}>{props.children}</PathProvider>;
 }
