feb3c17b396ef06cc493fc067ef0857f7d8fb6e2
Joe George <joe@zeroc.com>
2025-12-04 15:21:36 -0500

    fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,56 +6,19 @@ import tsparser from '@typescript-eslint/parser';
 
 export default [
   {
-    ignores: [
-      '.next/**',
-      'node_modules/**',
-      'out/**',
-      'build/**',
-      '*.config.js',
-      '*.config.mjs',
-      'utils/prism-*.js'
-    ]
+    ignores: ['.next/**', 'out/**', 'build/**', 'utils/prism-*.js']
   },
   js.configs.recommended,
   {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
-    plugins: { prettier },
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        React: 'readonly',
-        JSX: 'readonly'
-      }
-    },
-    rules: {
-      'prettier/prettier': 'error',
-      'no-unused-vars': 'warn',
-      'no-undef': 'off'
-    }
-  },
-  {
-    files: ['**/*.{ts,tsx}'],
     plugins: { '@typescript-eslint': tseslint, prettier },
     languageOptions: {
       parser: tsparser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        React: 'readonly',
-        JSX: 'readonly'
-      },
-      parserOptions: { ecmaFeatures: { jsx: true } }
+      globals: { ...globals.browser, ...globals.node }
     },
     rules: {
       'prettier/prettier': 'error',
       'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      'no-undef': 'off'
+      '@typescript-eslint/no-unused-vars': 'warn'
     }
   }
 ];
