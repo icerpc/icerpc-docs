936a5d5a68c2f88eca18f54f25996b387c95c42c
Joe George <joe@zeroc.com>
2026-04-27 13:41:10 -0400

    Replace ElementRef with ComponentRef in tooltip
    'ElementRef' is deprecated

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -16,7 +16,7 @@ const TooltipTrigger = TooltipPrimitive.Trigger;
 const TooltipPortal = TooltipPrimitive.Portal;
 
 const TooltipContent = React.forwardRef<
-  React.ElementRef<typeof TooltipPrimitive.Content>,
+  React.ComponentRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
 >(({ className, sideOffset = 4, ...props }, ref) => (
   <TooltipPrimitive.Content
